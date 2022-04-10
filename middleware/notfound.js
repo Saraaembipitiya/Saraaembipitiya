09b27252d87d07b1be5a13759a5d8bfe655d00cc
Saraa <sathsarasenanayaka96@gmail.com>
2022-04-10 06:06:02 +0530

    updated

--- middleware/notfound.js
+++ middleware/notfound.js
@@ -2,11 +2,11 @@ const notFound = (req,res,next) =>{
     res.write("<h1>Page Not Found</h1>")
     res.write("<h2>Login:/api/v1/users/login</h2>")
     res.write("<h4 style='color:blue'>Please provide email and password for login </h4>")
-    res.write("<h2>Register:users/register</h2>")
+    res.write("<h2>Register:/api/v1/users/register</h2>")
     res.write("<h4 style='color:blue'>Please use name,email,password,street,apartment,zip,city,country,phone to register</h4>")
-    res.write("<h2>:Get-All-Products:products/</h2>")
-    res.write("<h2>:Get-Single-Product:products/:id</h2>")
-    res.write("<h2>:Get-Single-Product:products/featured</h2>")
+    res.write("<h2>:Get-All-Products:/api/v1/products/</h2>")
+    res.write("<h2>:Get-Single-Product:/api/v1/products/:id</h2>")
+    res.write("<h2>:Get-Single-Product:/api/v1/products/featured</h2>")
     res.write("<h4 style='color:red'>Please use API tester for tesing,  Admin routes are not in there, All the product updating and deleting is done by admin</h4>")
 
     
