01df9ade15d4c125434db7fd36e4442045363463
Saraa <sathsarasenanayaka96@gmail.com>
2022-04-10 05:42:42 +0530

    updated

--- middleware/notfound.js
+++ middleware/notfound.js
@@ -1,5 +1,9 @@
 const notFound = (req,res,next) =>{
-    res.send("<h1>Page Not Found</h1>")
+    res.write("<h1>Page Not Found</h1>")
+    res.write("<h2>Login:users/login</h2>")
+    res.write("<h2>Register:users/register</h2>")
+    res.send()
+
 }
 
 
