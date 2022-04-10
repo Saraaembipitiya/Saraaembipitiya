fb11b1df803d4cbd4caeeb16bd386a1b934a81ce
Saraa <sathsarasenanayaka96@gmail.com>
2022-04-10 06:04:43 +0530

    updated

--- middleware/notfound.js
+++ middleware/notfound.js
@@ -1,6 +1,6 @@
 const notFound = (req,res,next) =>{
     res.write("<h1>Page Not Found</h1>")
-    res.write("<h2>Login:users/login</h2>")
+    res.write("<h2>Login:/api/v1/users/login</h2>")
     res.write("<h4 style='color:blue'>Please provide email and password for login </h4>")
     res.write("<h2>Register:users/register</h2>")
     res.write("<h4 style='color:blue'>Please use name,email,password,street,apartment,zip,city,country,phone to register</h4>")
